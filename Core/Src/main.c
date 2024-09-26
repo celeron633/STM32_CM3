672e05544ec657beef0b2f682ee4af96c4181821
celeron633 <dengxionghui@hotmail.com>
2024-09-26 23:59:10 +0800

    STM32_CM3: LDR, LDRH

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -60,6 +60,91 @@ int __io_putchar(int ch)
   HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
   return 1;
 }
+
+void setValue(int *ptr, int val)
+{
+  asm volatile
+  (
+    "ldr r0, %1\n"
+    "str r0, [%0]\n"
+    :
+    :"p"(ptr), "m"(val)
+    : "r0"
+  );
+}
+
+void setValue2(int *ptr, int val)
+{
+  asm volatile
+  (
+    "mov r0, %1\n"
+    "str r0, [%0]\n"
+    :
+    :"r"(ptr), "r"(val)
+    : "r0", "r1"
+  );
+}
+
+void ldrTest(void)
+{
+  uint8_t arr[] = {0x12, 0x34, 0x56, 0x78};
+  int outVal = 0;
+
+  printf("&arr=0x%08x\r\n", (int)arr);
+
+  asm volatile
+  (
+    "mov r0, %1\n"
+    "ldr r1, [r0]\n"
+    "mov %0, r1\n"
+    :"=r"(outVal)
+    :"r"(arr)
+    :"r0", "r1"
+  );
+  printf("outVal=0x%08x\r\n", outVal);
+  // outVal=0x78563412
+
+  outVal = 0;
+  asm volatile
+  (
+    "mov r0, %1\n"
+    "ldrh r1, [r0]\n"
+    "mov %0, r1\n"
+    :"=r"(outVal)
+    :"r"(arr)
+    :"r0", "r1"
+  );
+  printf("outVal=0x%08x\r\n", outVal);
+  // outVal=0x00003412
+
+  outVal = 0;
+  int arrAddr = (int)arr;
+  asm volatile
+  (
+    "ldr r0, %1\n"
+    "ldr r1, [r0]\n"
+    "mov %0, r1\n"
+    :"=r"(outVal)
+    :"m"(arrAddr)
+    :"r0"
+  );
+  printf("outVal=0x%08x\r\n", outVal);
+  // outVal=0x78563412
+
+  outVal = 0;
+  char *arrAddr2 = (char *)&arr;
+  asm volatile
+  (
+    "ldr r0, %1\n"
+    "mov %0, r0\n"
+    :"=r"(outVal)
+    :"m"(arrAddr2)
+    :"r0"
+  );
+  printf("outVal=0x%08x\r\n", outVal);
+  // outVal=0x2000ffd0
+
+}
 /* USER CODE END 0 */
 
 /**
@@ -125,6 +210,17 @@ int main(void)
     printf("msp=%08x, psp=%08x\r\n", mspVal, pspVal);
     printf("inputListVal=%08x\r\n", inputListVal);
     printf("xPSR=%08x\r\n", xPSRVal);
+
+
+    int testVal = 0;
+    printf("testVal=%d\r\n", testVal);
+    setValue(&testVal, 100);
+    printf("testVal=%d\r\n", testVal);
+    setValue2(&testVal, 200);
+    printf("testVal=%d\r\n", testVal);
+
+    ldrTest();
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
